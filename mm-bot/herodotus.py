6c55018cb0016d4b31c922eaa0254717ee5897fa
d-roak <me+git@droak.sh>
2023-11-09 14:35:00 +0900

    initial mm-bot version

---
+++ mm-bot/herodotus.py
@@ -0,0 +1,74 @@
+import constants
+import requests
+import os
+
+def herodotus_prove(block) -> str:
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    request_data = {
+    	"destinationChainId": HERODOTUS_DESTINATION_CHAIN,
+    	"fee": "0",
+    	"data": {
+    		HERODOTUS_ORIGIN_CHAIN: {
+                "block:{}".format(block): {
+    				"header": [
+    					"PARENT_HASH",
+    					"TRANSACTIONS_ROOT"
+    				],
+    				"accounts": {
+    					constants.ETH_CONTRACT_ADDR: {
+    						"props": ["STORAGE_ROOT"]
+    					}
+    				}
+    			}
+    		}
+    	}
+    }
+
+    retries = 0
+    while retries <= MAX_RETRIES:
+        try:
+            response = requests.post(
+                'https://api.herodotus.cloud/submit-batch-query?apiKey={}'.format(HERODOTUS_API_KEY),
+                headers=headers,
+                json=request_data,
+            )
+            response.raise_for_status()
+        
+            return response.json()['internalId']
+        except requests.exceptions.RequestException as err:
+            retries += 1
+            if retries == MAX_RETRIES:
+                raise err
+            time.sleep(RETRIES_DELAY)
+
+def herodotus_status(task_id) -> str:
+    try:
+        response = requests.get(
+            'https://api.herodotus.cloud/batch-query-status?apiKey={}&batchQueryId={}'.format(HERODOTUS_API_KEY, task_id),
+        )
+        response.raise_for_status()
+    
+        return response.json()['queryStatus']
+    except requests.exceptions.RequestException as err:
+        raise err
+
+def herodotus_poll_status(task_id) -> bool:
+    # instead of returning a bool we can store it in a mapping
+    retries = 0
+    while retries <= MAX_RETRIES:
+        try:
+            status = herodotus_status(task_id)
+            if status == 'DONE':
+                return True
+            retries += 1
+            time.sleep(RETRIES_DELAY)
+        except requests.exceptions.RequestException as err:
+            retries += 1
+            if retries == MAX_RETRIES:
+                raise err
+            time.sleep(RETRIES_DELAY)
+
+    return False
---
+++ mm-bot/main.py
@@ -0,0 +1,15 @@
+import herodotus
+
+if __name__ == '__main__':
+    print(herodotus.ping())
+    # 1. Listen event on starknet
+    
+    # 2. Transfer eth on ethereum
+    # (bridging is complete for the user)
+
+    # 3. Call herodotus to prove
+
+    # 4. Poll herodotus to check task status
+
+    # 5. Withdraw eth from starknet
+    # (bridging is complete for the mm)
