ad4217a6a76b96a723f32cdcc7c6b5cf4020b793
d-roak <me+git@droak.sh>
2023-11-09 16:51:44 +0900

    rm frontend for now

--- app/src/pages/api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
-  res.status(200).json({ data: 'Alive' })
-}
--- app/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Bridge from '@/components/bridge';
-
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center w-full mt-16">
-      <Bridge />
-    </div>
-  )
-}
