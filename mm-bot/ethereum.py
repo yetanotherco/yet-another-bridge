fe3c0877d8fd20f8e0a80cb0f09fa831bccf04bd
rcatalan98 <robertitocatalan@gmail.com>
2023-11-09 23:34:42 -0300

    fix: transfer function from mm to user

--- mm-bot/ethereum.py
+++ mm-bot/ethereum.py
@@ -8,15 +8,18 @@ w3 = Web3(Web3.HTTPProvider(constants.ETH_RPC_URL))
 def get_latest_block() -> int:
     return w3.eth.block_number
 
-def transfer(dst_addr, amount):
+def transfer(deposit_id, dst_addr, amount):
     acct = w3.eth.account.from_key(constants.ETH_PRIVATE_KEY)
     # get only the abi not the entire file
     abi = json.load(open('abi/YABTransfer.json'))['abi']
 
     yab_transfer = w3.eth.contract(address=constants.ETH_CONTRACT_ADDR, abi=abi)
-
+    dst_addr_bytes = Web3.to_int(hexstr=dst_addr)
+    deposit_id = Web3.to_int(deposit_id)
+    amount = Web3.to_int(amount)
     # we need amount so the transaction is valid with the trasnfer that will be transfered
-    unsent_tx = yab_transfer.functions.transfer(dst_addr, amount).build_transaction({
+    unsent_tx = yab_transfer.functions.transfer(deposit_id, dst_addr_bytes, amount).build_transaction({
+        "chainId": 5,
         "from": acct.address,
         "nonce": w3.eth.get_transaction_count(acct.address),
         "value": amount,
@@ -24,4 +27,5 @@ def transfer(dst_addr, amount):
     signed_tx = w3.eth.account.sign_transaction(unsent_tx, private_key=acct.key)
     
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(w3.eth.wait_for_transaction_receipt(tx_hash))
+    
