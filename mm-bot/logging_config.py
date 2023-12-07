9b0e549b18e893dcaf93ed5723f7efe17f0ad0a2
Julián Arce <juarce@itba.edu.ar>
2023-12-07 19:53:42 -0300

    refactor: modularize logger config methods

--- mm-bot/logging_config.py
+++ mm-bot/logging_config.py
@@ -12,12 +12,28 @@ def setup_logger():
     log_format = "%(asctime)s - [%(levelname)s] - %(message)s"
     formatter = logging.Formatter(log_format, datefmt="%Y-%m-%dT%H:%M:%S")
 
-    # Console Handler for development
+    # Add handlers based on the environment
+    if in_production():
+        handler = get_production_handler(formatter)
+    else:
+        handler = get_development_handler(formatter)
+    logger.addHandler(handler)
+
+
+def in_production():
+    return constants.ENVIRONMENT == 'prod'
+
+
+# Console Handler for development
+def get_development_handler(formatter):
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setLevel(constants.LOGGING_LEVEL)
     console_handler.setFormatter(formatter)
+    return console_handler
 
-    # File Handler for production with daily rotation
+
+# File Handler for production with daily rotation
+def get_production_handler(formatter):
     file_handler = TimedRotatingFileHandler(
         filename=f"{constants.LOGGING_DIR}/mm-bot.log",
         when="midnight",
@@ -27,13 +43,4 @@ def setup_logger():
     )
     file_handler.setLevel(constants.LOGGING_LEVEL)
     file_handler.setFormatter(formatter)
-
-    # Add handlers based on the environment
-    if in_production():
-        logger.addHandler(file_handler)
-    else:
-        logger.addHandler(console_handler)
-
-
-def in_production():
-    return constants.ENVIRONMENT == 'prod'
+    return file_handler
