c6a3ec60294b34122a22f193395b89428eb1c051
dubzn <santiagodgalvan@gmail.com>
2024-01-08 15:33:44 -0300

    remove SN_ESCROW_ADDRESS from envs

--- contracts/solidity/script/Deploy.s.sol
+++ contracts/solidity/script/Deploy.s.sol
@@ -8,8 +8,8 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 contract Deploy is Script {
     function run() external returns (address) {
         address snMessagingAddress = vm.envAddress("SN_MESSAGING_ADDRESS");
-        uint256 snEscrowAddress = vm.envUint("SN_ESCROW_ADDRESS");
         uint256 snWithdrawSelector = vm.envUint("SN_WITHDRAW_SELECTOR");
+        uint256 snEscrowAddress = 0x0;
         vm.startBroadcast();
 
         YABTransfer yab = new YABTransfer();
