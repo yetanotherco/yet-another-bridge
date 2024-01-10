b8a69f8b2d04d91f06573282cba25d264cc423d1
Uriel Mihura <uriel.mihura@gmail.com>
2024-01-10 17:33:57 -0300

    ignore comments fix in .env import

--- contracts/solidity/deploy.sh
+++ contracts/solidity/deploy.sh
@@ -9,9 +9,9 @@ cd "$(dirname "$0")"
 load_env() {
     unamestr=$(uname)
     if [ "$unamestr" = 'Linux' ]; then
-      export $(grep -v '^#' .env | xargs -d '\n')
+      export $(sed '/^#/d; s/#.*$//' .env | xargs -d '\n')
     elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
-      export $(grep -v '^#' .env | xargs -0)
+      export $(sed '/^#/d; s/#.*$//' .env | xargs -0)
     fi
 }
 load_env
