9590547bd6d270f342d94897018ab7a81dd1df1a
Julián Arce <juarce@itba.edu.ar>
2024-01-10 21:43:35 -0300

    fix: use clock_timestamp instead of current_timestamp

--- mm-bot/src/models/block.py
+++ mm-bot/src/models/block.py
@@ -9,7 +9,7 @@ class Block(Base):
     __tablename__ = "block"
     id: int = Column(Integer, primary_key=True, nullable=False)
     latest_block: int = Column(Integer, nullable=False)
-    created_at: datetime = Column(DateTime, nullable=False, default=datetime.now())
+    created_at: datetime = Column(DateTime, nullable=False, server_default="clock_timestamp()")
 
     def __str__(self):
         return f"latest_block:{self.latest_block}"
