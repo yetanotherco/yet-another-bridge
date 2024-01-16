c1e09136589d76953a5d55dac4d6ff1c4f816b9b
Santos Rosati <rosatisantos@gmail.com>
2024-01-16 13:13:35 -0300

    refactor: remove transfer.sh

--- mm-bot/transfer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-approve=124000000000000
-amount=124000000000000
-
-starkli invoke eth approve 0x07b436a3148e98d4e9fb5d535c45526d06495d24421a24becfc7361884371613 u256:$approve / 0x07b436a3148e98d4e9fb5d535c45526d06495d24421a24becfc7361884371613 set_order 0x769383FbA3f8c007D19C8FE57cF4422A93522b84 u256:$amount u256:0 --keystore ~/.starkli-wallets/keystore.json --account ~/.starkli-wallets/account.json
