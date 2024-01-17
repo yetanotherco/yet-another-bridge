44d7033eb505b0dca38efd8857cc081c28ec3f63
Uriel Mihura <uriel.mihura@gmail.com>
2024-01-17 17:31:19 -0300

    set eth proxy addr in cairo/env

--- contracts/solidity/deploy.sh
+++ contracts/solidity/deploy.sh
@@ -25,10 +25,18 @@ YAB_TRANSFER_ADDRESS=$(echo "$RESULT_LOG" | grep -Eo '1: address ([^\n]+)' | awk
 echo -e "${GREEN}\n=> [ETH] Deployed Proxy address: $PROXY_ADDRESS ${COLOR_RESET}"
 echo -e "${GREEN}\n=> [ETH] Deployed YABTransfer address: $YAB_TRANSFER_ADDRESS ${COLOR_RESET}"
 
-# Setting proxy_address into .env file
+# Setting proxy_address into solidity/.env file
 if grep -q "^YAB_TRANSFER_PROXY_ADDRESS=" ".env"; then
   sed "s/^YAB_TRANSFER_PROXY_ADDRESS=.*/YAB_TRANSFER_PROXY_ADDRESS=$PROXY_ADDRESS/" .env >> env.temp.file
   mv env.temp.file .env
 else
   echo "YAB_TRANSFER_PROXY_ADDRESS=$PROXY_ADDRESS" >> ".env"
+fi
+
+# Setting proxy_address into cairo/.env file
+if grep -q "^YAB_TRANSFER_PROXY_ADDRESS=" "../cairo/.env"; then
+  sed "s/^YAB_TRANSFER_PROXY_ADDRESS=.*/YAB_TRANSFER_PROXY_ADDRESS=$PROXY_ADDRESS/" ../cairo/.env >> ../cairo/env.temp.file
+  mv ../cairo/env.temp.file ../cairo/.env
+else
+  echo "YAB_TRANSFER_PROXY_ADDRESS=$PROXY_ADDRESS" >> "../cairo/.env"
 fi
