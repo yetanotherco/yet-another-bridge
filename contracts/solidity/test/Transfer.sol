97162e60a93202c707f45df307dbe83cb27a17cc
rcatalan98 <robertitocatalan@gmail.com>
2024-01-17 15:48:02 -0300

    fix: change transfer test to proxy format

--- contracts/solidity/test/Transfer.sol
+++ contracts/solidity/test/Transfer.sol
@@ -5,24 +5,26 @@ import "forge-std/Test.sol";
 import "../src/YABTransfer.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-
 contract TransferTest is Test {
     YABTransfer public yab;
+    address public marketMaker;
+    ERC1967Proxy public proxy;
 
     function setUp() public {
         address snMessagingAddress = 0xde29d060D45901Fb19ED6C6e959EB22d8626708e;
         uint256 snEscrowAddress = 0x0;
         uint256 snEscrowWithdrawSelector = 0x15511cc3694f64379908437d6d64458dc76d02482052bfb8a5b33a72c054c77;
-        address MarketMaker = 0xda963fA72caC2A3aC01c642062fba3C099993D56;
+        marketMaker = 0xda963fA72caC2A3aC01c642062fba3C099993D56;
         
         yab = new YABTransfer();
-        ERC1967Proxy proxy = new ERC1967Proxy(address(yab), "");
-        YABTransfer(address(proxy)).initialize(snMessagingAddress, snEscrowAddress, snEscrowWithdrawSelector, MarketMaker);
+        proxy = new ERC1967Proxy(address(yab), "");
+        YABTransfer(address(proxy)).initialize(snMessagingAddress, snEscrowAddress, snEscrowWithdrawSelector, marketMaker);
     }
 
     // Test cases are WIP
 
     function testTransfer() public {
-        yab.transfer{value: 100}(1, 0x1, 100);
+        hoax(marketMaker, 100 wei);
+        YABTransfer(address(proxy)).transfer{value: 100}(1, 0x1, 100);
     }
 }
