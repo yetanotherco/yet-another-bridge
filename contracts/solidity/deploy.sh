5414d5e7b802068a468193ca9f9e7bd00bb1a178
Julián Arce <juarce@itba.edu.ar>
2024-01-24 15:43:36 -0300

    build: deploy YABTransfer

--- contracts/solidity/deploy.sh
+++ contracts/solidity/deploy.sh
@@ -6,13 +6,13 @@ COLOR_RESET='\033[0m'
 
 cd "$(dirname "$0")"
 
-if [ -f .env ]; then
-    echo "Sourcing solidity/.env file..."
-    source .env
-else
-    echo "Error: solidity/.env file not found!"
-    exit 1
-fi
+#if [ -f .env ]; then
+#    echo "Sourcing solidity/.env file..."
+#    source .env
+#else
+#    echo "Error: solidity/.env file not found!"
+#    exit 1
+#fi
 
 echo -e "${GREEN}\n=> [ETH] Deploy Escrow${COLOR_RESET}"
 forge script ./script/Deploy.s.sol --fork-url $ETH_RPC_URL --broadcast --verify -vvvv
