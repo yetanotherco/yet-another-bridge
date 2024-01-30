03f0d3db651c336133d553bdccec91db3fd38533
JuArce <jarcedoncella@gmail.com>
2024-01-30 19:00:31 -0300

    build(test.yml): set eth_contract as variable in transfer.sh and withdraw.sh

--- .github/workflows/scripts/transfer.sh
+++ .github/workflows/scripts/transfer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 amount=124000000000000
+eth_contract=0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
 
 echo -e "${GREEN}\n=> [SN] Making transfer to Destination account${COLOR_RESET}" # 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 -> 642829559307850963015472508762062935916233390536
 
 echo "Initial balance:"
 cast balance --rpc-url $ETH_RPC_URL --ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 echo "Transferring $amount to 0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-#cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "transfer(ether, uint256, uint256, uint256)" "0.000124" "0" "642829559307850963015472508762062935916233390536" "124000000000000"
-cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "transfer(uint256, uint256, uint256)" "0" "642829559307850963015472508762062935916233390536" "$amount" --value $amount
+cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY $eth_contract "transfer(uint256, uint256, uint256)" "0" "642829559307850963015472508762062935916233390536" "$amount" --value $amount
 echo "Final balance:"
 cast balance --rpc-url $ETH_RPC_URL --ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
--- .github/workflows/scripts/withdraw.sh
+++ .github/workflows/scripts/withdraw.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 amount=124000000000000
+eth_contract=0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
 
-echo -e "${GREEN}\n=> [SN] Making transfer to Destination account${COLOR_RESET}" # 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 -> 642829559307850963015472508762062935916233390536
+echo -e "${GREEN}\n=> [SN] Making withdraw${COLOR_RESET}" # 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 -> 642829559307850963015472508762062935916233390536
 
 echo "Withdrawing $amount"
-cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "withdraw(uint256, uint256, uint256)" "0" "642829559307850963015472508762062935916233390536" "$amount" --value $amount
+cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY $eth_contract "withdraw(uint256, uint256, uint256)" "0" "642829559307850963015472508762062935916233390536" "$amount" --value $amount
