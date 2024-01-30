f52f9c74632206b7fb2965f5ad0492775ef43ae1
Uriel Mihura <uriel.mihura@gmail.com>
2024-01-30 13:24:17 -0300

    style: printed info after make-all

--- contracts/display_info.sh
+++ contracts/display_info.sh
@@ -1,4 +1,4 @@
-echo "\nNewly deployed contracts information:"
-printf "${GREEN}=> [ETH] Deployed Proxy address: $YAB_TRANSFER_PROXY_ADDRESS ${COLOR_RESET}\n"
-printf "${GREEN}=> [ETH] Deployed YABTransfer address: $YAB_TRANSFER_ADDRESS ${COLOR_RESET}\n"
+printf "${GREEN}\n=> Newly deployed contracts information: ${COLOR_RESET}\n"
+printf "${PINK}[ETH] Deployed Proxy address: $YAB_TRANSFER_PROXY_ADDRESS ${COLOR_RESET}\n"
+printf "${PINK}[ETH] Deployed YABTransfer address: $YAB_TRANSFER_ADDRESS ${COLOR_RESET}\n"
 printf "${CYAN}[SN] Escrow Address: $ESCROW_CONTRACT_ADDRESS${COLOR_RESET}\n"
