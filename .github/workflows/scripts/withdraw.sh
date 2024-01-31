6b4c736826001f7b7700bff0307f9473be21112f
JuArce <jarcedoncella@gmail.com>
2024-01-31 17:08:06 -0300

    build(withdraw.sh): sleep 60 seconds before check withdraw status

--- .github/workflows/scripts/withdraw.sh
+++ .github/workflows/scripts/withdraw.sh
@@ -6,7 +6,9 @@ RESET='\e[0m'
 
 echo -e "${GREEN}\n=> [SN] Making withdraw${COLOR_RESET}" # 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 -> 642829559307850963015472508762062935916233390536
 
+ESCROW_INITIAL_BALANCE=$(starkli balance $ESCROW_CONTRACT_ADDRESS)
 MM_INITIAL_BALANCE=$(starkli balance $MM_SN_WALLET_ADDR)
+echo "Initial Escrow balance: $ESCROW_INITIAL_BALANCE"
 echo "Initial MM balance: $MM_INITIAL_BALANCE"
 
 echo "Withdrawing $amount"
@@ -15,7 +17,10 @@ cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY \
   "0" "642829559307850963015472508762062935916233390536" "$AMOUNT" \
   --value $AMOUNT >> /dev/null
 
+sleep 60
 starkli call $ESCROW_CONTRACT_ADDRESS get_order_used u256:0
 
+ESCROW_FINAL_BALANCE=$(starkli balance $ESCROW_CONTRACT_ADDRESS)
 MM_FINAL_BALANCE=$(starkli balance $MM_SN_WALLET_ADDR)
+echo "Final Escrow balance: $ESCROW_FINAL_BALANCE"
 echo "Final MM balance: $MM_FINAL_BALANCE"
