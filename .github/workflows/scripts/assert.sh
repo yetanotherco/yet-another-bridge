e342f985a8fcb91b3696e438e8c872b5f1fc90f7
JuArce <jarcedoncella@gmail.com>
2024-01-31 18:21:25 -0300

    fix(assert.sh): use eval operator

--- .github/workflows/scripts/assert.sh
+++ .github/workflows/scripts/assert.sh
@@ -8,15 +8,15 @@ RESET='\e[0m'
 # Define the assert function
 assert() {
   # Usage: assert <condition> <placeholder_text> <expected_value> <obtained_value>
-  if "$1"; then
-    echo "${GREEN}$2 passed.${RESET}"
+  if eval "$1"; then
+    printf "${GREEN}$2 passed.${RESET}\n"
   else
-    echo "${RED}$2 assertion failed: Expected value: $3, Obtained value: $4${RESET}"
+    printf "${RED}$2 assertion failed: Expected value: $3, Obtained value: $4${RESET}\n"
     exit 1
   fi
 }
 
 DESTINATION_FINAL_BALANCE=$(cast balance --rpc-url $ETH_RPC_URL $DESTINATION_ADDRESS)
 EXPECTED_DESTINATION_FINAL_BALANCE=10001000000000000000000
-assert "[[ $DESTINATION_FINAL_BALANCE -eq $expected_DESTINATION_FINAL_BALANCE ]]" "Destination balance" "10001000000000000000000" "$DESTINATION_FINAL_BALANCE"
+assert "[[ $DESTINATION_FINAL_BALANCE -eq $EXPECTED_DESTINATION_FINAL_BALANCE ]]" "Destination balance" "10001000000000000000000" "$DESTINATION_FINAL_BALANCE"
 
