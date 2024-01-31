4af4227bdeb41afa2c2a3dbae1c047a3ccff58ef
JuArce <jarcedoncella@gmail.com>
2024-01-31 12:00:22 -0300

    build(transfer.sh): check ethereum balances from mm and destination addresses

--- .github/workflows/scripts/transfer.sh
+++ .github/workflows/scripts/transfer.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 amount=124000000000000
-eth_contract=$YAB_TRANSFER_PROXY_ADDRESS
+DESTINATION_ADDRESS=642829559307850963015472508762062935916233390536
 
 echo -e "${GREEN}\n=> [SN] Making transfer to Destination account${COLOR_RESET}" # 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 -> 642829559307850963015472508762062935916233390536
 
-echo "Initial balance:"
-cast balance --rpc-url $ETH_RPC_URL --ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+MM_INITIAL_BALANCE=$(cast balance --rpc-url $ETH_RPC_URL --ether $MM_ETHEREUM_WALLET)
+DESTINATION_INITIAL_BALANCE=$(cast balance --rpc-url $ETH_RPC_URL --ether $DESTINATION_ADDRESS)
+echo "Initial MM balance: $MM_INITIAL_BALANCE"
+echo "Initial Destination balance: $DESTINATION_INITIAL_BALANCE"
+
 echo "Transferring $amount to 0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY $eth_contract "transfer(uint256, uint256, uint256)" "0" "642829559307850963015472508762062935916233390536" "$amount" --value $amount
-echo "Final balance:"
-cast balance --rpc-url $ETH_RPC_URL --ether 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY $YAB_TRANSFER_PROXY_ADDRESS "transfer(uint256, uint256, uint256)" "0" "$DESTINATION_ADDRESS" "$amount" --value $amount
+
+MM_FINAL_BALANCE=$(cast balance --rpc-url $ETH_RPC_URL --ether $MM_ETHEREUM_WALLET)
+DESTINATION_FINAL_BALANCE=$(cast balance --rpc-url $ETH_RPC_URL --ether $DESTINATION_ADDRESS)
+echo "Final MM balance: $MM_FINAL_BALANCE"
+echo "Final Destination balance: $DESTINATION_FINAL_BALANCE"
