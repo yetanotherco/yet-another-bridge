dabaf3d45aaafe1e6d5759676e68d250af84863e
JuArce <52429267+JuArce@users.noreply.github.com>
2024-02-26 13:30:35 -0300

    refactor: standardize env file names

--- .github/workflows/scripts/claim_payment.sh
+++ .github/workflows/scripts/claim_payment.sh
@@ -5,12 +5,12 @@
 echo -e "${GREEN}\n=> [SN] Making withdraw${COLOR_RESET}" # 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 -> 642829559307850963015472508762062935916233390536
 
 ESCROW_INITIAL_BALANCE=$(starkli balance $ESCROW_CONTRACT_ADDRESS)
-MM_INITIAL_BALANCE=$(starkli balance $MM_SN_WALLET_ADDR)
+MM_INITIAL_BALANCE=$(starkli balance $MM_STARKNET_WALLET)
 echo "Initial Escrow balance: $ESCROW_INITIAL_BALANCE"
 echo "Initial MM balance: $MM_INITIAL_BALANCE"
 
 echo "Withdrawing $AMOUNT"
-cast send --rpc-url $ETH_RPC_URL --private-key $ETH_PRIVATE_KEY \
+cast send --rpc-url $ETHEREUM_RPC --private-key $ETHEREUM_PRIVATE_KEY \
   $PAYMENT_REGISTRY_PROXY_ADDRESS "claimPayment(uint256, uint256, uint256)" \
   "0" "642829559307850963015472508762062935916233390536" "$AMOUNT" \
   --value $AMOUNT >> /dev/null
@@ -20,6 +20,6 @@ sleep 15
 starkli call $ESCROW_CONTRACT_ADDRESS get_order_pending u256:0
 
 ESCROW_FINAL_BALANCE=$(starkli balance $ESCROW_CONTRACT_ADDRESS)
-MM_FINAL_BALANCE=$(starkli balance $MM_SN_WALLET_ADDR)
+MM_FINAL_BALANCE=$(starkli balance $MM_STARKNET_WALLET)
 echo "Final Escrow balance: $ESCROW_FINAL_BALANCE"
 echo "Final MM balance: $MM_FINAL_BALANCE"
