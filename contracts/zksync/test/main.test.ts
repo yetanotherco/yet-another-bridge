8cfaa2e754377e7a6af9664c9c369ab44bdab4f0
Urix <43704209+uri-99@users.noreply.github.com>
2024-03-01 17:41:07 -0300

    test: initial setOrder test

--- contracts/zksync/test/main.test.ts
+++ contracts/zksync/test/main.test.ts
@@ -1,17 +1,21 @@
 import { expect } from 'chai';
 import { deployAndInit } from './utils';
-import { Contract, Wallet } from 'ethers';
+import { Contract, Fragment, Wallet } from 'ethers';
 import { getWallet, deployContract, LOCAL_RICH_WALLETS } from '../deploy/utils';
 
 let escrow: Contract;
-let deployer: Wallet;
+let deployer: Wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
+let user_zk: Wallet = getWallet(LOCAL_RICH_WALLETS[1].privateKey);
+let user_eth: Wallet = getWallet(LOCAL_RICH_WALLETS[1].privateKey);
+
+const fee = 1; //TODO check, maybe make fuzz
+const value = 10; //TODO check, maybe make fuzz
 
 beforeEach( async () => {
   escrow = await deployAndInit();
-  deployer = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
 });
 
-
+// working:::
 describe('Pause tests', function () {
 
   it("Should start unpaused", async function () {
@@ -30,10 +34,48 @@ describe('Pause tests', function () {
     escrow.connect(deployer);
     const setPauseTx = await escrow.pause();
     await setPauseTx.wait();
-    
+
     const setUnpauseTx = await escrow.unpause();
     await setUnpauseTx.wait();
 
     expect(await escrow.paused()).to.eq(false);
   });
 });
+
+describe('Set Order tests', function () {
+
+  // working
+  it("Should emit correct Event", async () => {
+    escrow.connect(user_zk);
+
+    let events = await escrow.queryFilter("*");
+    const events_length = events.length;
+
+    const setOrderTx = await escrow.set_order(user_eth, fee, {value});
+    await setOrderTx.wait();
+    
+    events = await escrow.queryFilter("*");
+    expect(events.length).to.equal(events_length + 1);
+    expect(events[events.length - 1].fragment.name).to.equal("SetOrder");
+  });
+
+  // working
+  it("Should get the order setted", async () => {
+    escrow.connect(user_zk);
+
+    const setOrderTx = await escrow.set_order(user_eth, fee, {value});
+    await setOrderTx.wait();
+
+    let events = await escrow.queryFilter("*");
+    const newOrderEvent = events[events.length - 1];
+
+    const orderId = newOrderEvent.args[0];
+
+    const newOrder = await escrow.get_order(orderId);
+
+    expect(newOrder[0]).to.eq(user_eth.address); //recipient_address
+    expect(Number(newOrder[1])).to.eq(value-fee); //amount
+    expect(Number(newOrder[2])).to.eq(fee); //fee
+
+  })
+})
