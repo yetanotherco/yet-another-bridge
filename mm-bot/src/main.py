1362b02b9ec0aeb06322e008798687df73ccc739
JuArce <52429267+JuArce@users.noreply.github.com>
2024-03-07 15:57:08 -0300

    refactor: implement FailedOrdersProcessor refactor: process incomplete orders using OrderExecutor

--- mm-bot/src/main.py
+++ mm-bot/src/main.py
@@ -6,20 +6,17 @@ import schedule
 from config import constants
 from config.database_config import get_db
 from config.logging_config import setup_logger
-from models.order import Order
-from models.order_status import OrderStatus
 from persistence.block_dao import BlockDao
 from persistence.error_dao import ErrorDao
 from persistence.order_dao import OrderDao
-from services import ethereum
 from services.executors.order_executor import OrderExecutor
 from services.indexers.starknet_order_indexer import StarknetOrderIndexer
 from services.order_service import OrderService
-from services.overall_fee_calculator import estimate_overall_fee
 from services.payment_claimer.ethereum_payment_claimer import EthereumPaymentClaimer
 from services.payment_claimer.herodotus_payment_claimer import HerodotusPaymentClaimer
 from services.payment_claimer.payment_claimer import PaymentClaimer
 from services.processors.accepted_blocks_orders_processor import AcceptedBlocksOrdersProcessor
+from services.processors.failed_orders_processor import FailedOrdersProcessor
 from services.processors.orders_processor import OrdersProcessor
 from services.senders.ethereum_sender import EthereumSender
 
@@ -53,19 +50,21 @@ async def run():
                                    eth_lock, herodotus_semaphore, MAX_ETH_TRANSFER_WEI)
     orders_processor = OrdersProcessor(order_indexer, order_executor)
 
+    failed_orders_processor = FailedOrdersProcessor(order_executor, order_service)
     accepted_blocks_orders_processor = AcceptedBlocksOrdersProcessor(order_indexer, order_executor, block_dao)
 
     (schedule.every(PROCESS_NO_BALANCE_ORDERS_MINUTES_TIMER).minutes
-     .do(failed_orders_job, order_service, eth_lock, herodotus_semaphore))
+     .do(failed_orders_processor.process_orders_job))
     (schedule.every(PROCESS_ACCEPTED_BLOCKS_MINUTES_TIMER).minutes
      .do(accepted_blocks_orders_processor.process_orders_job))
+
     schedule.run_all()
 
     try:
         # Get all orders that are not completed from the db
         orders = order_service.get_incomplete_orders()
         for order in orders:
-            create_order_task(order, order_service, eth_lock, herodotus_semaphore)
+            order_executor.execute(order)
     except Exception as e:
         logger.error(f"[-] Error: {e}")
 
@@ -79,96 +78,6 @@ async def run():
             logger.error(f"[-] Error: {e}")
 
         await asyncio.sleep(SLEEP_TIME)
-
-
-def create_order_task(order: Order, order_service: OrderService, eth_lock: asyncio.Lock,
-                      herodotus_semaphore: asyncio.Semaphore):
-    asyncio.create_task(process_order(order, order_service, eth_lock, herodotus_semaphore),
-                        name=f"Order-{order.order_id}")
-
-
-async def process_order(order: Order, order_service: OrderService,
-                        eth_lock: asyncio.Lock, herodotus_semaphore: asyncio.Semaphore):
-    try:
-        logger.info(f"[+] Processing order: {order}")
-        # 1. Check if the order fee is enough
-        if order.status is OrderStatus.PENDING:
-            estimated_fee = await estimate_overall_fee(order)
-            if order.get_int_fee() < estimated_fee:
-                logger.error(f"[-] Order fee is too low: {order.get_int_fee()} < {estimated_fee}")
-                order_service.set_order_dropped(order)
-                return
-            order_service.set_order_processing(order)
-
-        # 1.5. Check if order amount is too high
-        if order.amount > MAX_ETH_TRANSFER_WEI:
-            logger.error(f"[-] Order amount is too high: {order.amount}")
-            order_service.set_order_dropped(order)
-            return
-
-        # 2. Transfer eth on ethereum
-        # (bridging is complete for the user)
-        if order.status in [OrderStatus.PROCESSING, OrderStatus.TRANSFERRING]:
-            async with eth_lock:
-                if order.status is OrderStatus.PROCESSING:
-                    await transfer(order, order_service)
-
-                # 2.5. Wait for transfer
-                if order.status is OrderStatus.TRANSFERRING:
-                    await wait_transfer(order, order_service)
-
-        if order.status in [OrderStatus.FULFILLED, OrderStatus.PROVING]:
-            async with herodotus_semaphore if using_herodotus() else eth_lock:
-                # 3. Call herodotus to prove
-                # extra: validate w3.eth.get_storage_at(addr, pos) before calling herodotus
-                if order.status is OrderStatus.FULFILLED:
-                    await payment_claimer.send_payment_claim(order, order_service)
-
-                # 4. Poll herodotus to check task status
-                if order.status is OrderStatus.PROVING:
-                    await payment_claimer.wait_for_payment_claim(order, order_service)
-
-        # 5. Claim payment eth from starknet
-        # (bridging is complete for the mm)
-        if order.status is OrderStatus.PROVED:
-            await payment_claimer.close_payment_claim(order, order_service)
-
-        if order.status is OrderStatus.COMPLETED:
-            logger.info(f"[+] Order {order.order_id} completed")
-    except Exception as e:
-        order_service.set_order_failed(order, str(e))
-
-
-def failed_orders_job(order_service: OrderService,
-                      eth_lock: asyncio.Lock, herodotus_semaphore: asyncio.Semaphore):
-    asyncio.create_task(process_failed_orders(order_service, eth_lock, herodotus_semaphore), name="No-balance-orders")
-
-
-async def process_failed_orders(order_service: OrderService,
-                                eth_lock: asyncio.Lock, herodotus_semaphore: asyncio.Semaphore):
-    try:
-        logger.debug(f"[+] Processing no balance orders")
-        orders = order_service.get_failed_orders()
-        for order in orders:
-            order_service.reset_failed_order(order)
-            create_order_task(order, order_service, eth_lock, herodotus_semaphore)
-    except Exception as e:
-        logger.error(f"[-] Error: {e}")
-
-
-async def transfer(order: Order, order_service: OrderService):
-    logger.info(f"[+] Transferring eth on ethereum")
-    # in case it's processed on ethereum, but not processed on starknet
-    tx_hash = await asyncio.to_thread(ethereum.transfer, order.order_id, order.recipient_address,
-                                      order.get_int_amount())
-    order_service.set_order_transferring(order, tx_hash)
-    logger.info(f"[+] Transfer tx hash: {tx_hash.hex()}")
-
-
-async def wait_transfer(order: Order, order_service: OrderService):
-    await asyncio.to_thread(ethereum.wait_for_transaction_receipt, order.transfer_tx_hash)
-    order_service.set_order_fulfilled(order)
-    logger.info(f"[+] Transfer complete")
 
 
 if __name__ == '__main__':
---
+++ mm-bot/src/services/processors/failed_orders_processor.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from services.executors.order_executor import OrderExecutor
+from services.order_service import OrderService
+
+
+class FailedOrdersProcessor:
+
+    def __init__(self, order_executor: OrderExecutor,
+                 order_service: OrderService):
+        self.logger = logging.getLogger(__name__)
+        self.order_executor: OrderExecutor = order_executor
+        self.order_service: OrderService = order_service
+
+    async def process_orders(self):
+        """
+        Process failed orders stored in the database
+        """
+        try:
+            orders = self.order_service.get_failed_orders()
+            for order in orders:
+                self.order_service.reset_failed_order(order)
+                self.order_executor.execute(order)
+        except Exception as e:
+            self.logger.error(f"[-] Error: {e}")
+
+    def process_orders_job(self):
+        """
+        Process orders job for the scheduler
+        """
+        asyncio.create_task(self.process_orders(),
+                            name="Failed Orders")
