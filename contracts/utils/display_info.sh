6e9df503471e696603eaded98b0c85b715f2fb8a
Urix <43704209+uri-99@users.noreply.github.com>
2024-03-22 16:56:27 -0300

    refactor: style \n at EOF

--- contracts/utils/display_info.sh
+++ contracts/utils/display_info.sh
@@ -1,6 +1,5 @@
 printf "${GREEN}\n=> Newly deployed contracts information: ${COLOR_RESET}\n"
-printf "${PINK}[ETH] Deployed Proxy address: $PAYMENT_REGISTRY_PROXY_ADDRESS ${COLOR_RESET}\n"
-printf "${PINK}[ETH] Deployed PaymentRegistry address: $PAYMENT_REGISTRY_ADDRESS ${COLOR_RESET}\n"
+printf "${PINK}[ETH] Deployed Proxy of PaymentRegistry address: $PAYMENT_REGISTRY_PROXY_ADDRESS ${COLOR_RESET}\n"
 
 if ! [ -z "$ESCROW_CONTRACT_ADDRESS" ]; then
     printf "${CYAN}[SN] Escrow Address: $ESCROW_CONTRACT_ADDRESS${COLOR_RESET}\n"
