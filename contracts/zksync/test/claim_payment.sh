30b2e8e1980885e3076812f6a8bbf6d9ccd9f4e6
Urix <43704209+uri-99@users.noreply.github.com>
2024-04-10 19:09:22 -0300

    feat: add assert.sh at the end of CI

--- contracts/zksync/test/claim_payment.sh
+++ contracts/zksync/test/claim_payment.sh
@@ -4,7 +4,7 @@
 
 printf "${GREEN}\n=> [ETH] Making Claim Payment${COLOR_RESET}\n"
 
-MM_INITIAL_BALANCE_L1=$(cast balance --rpc-url $ETHEREUM_RPC --ether $MM_ETHEREUM_WALLET_ADDRESS)
+export MM_INITIAL_BALANCE_L1=$(cast balance --rpc-url $ETHEREUM_RPC --ether $MM_ETHEREUM_WALLET_ADDRESS)
 echo "Initial MM balance L1:"
 echo "$MM_INITIAL_BALANCE_L1"
 
@@ -23,9 +23,9 @@ cast send --rpc-url $ETHEREUM_RPC --private-key $ETHEREUM_PRIVATE_KEY --gas-pric
   "0" $USER_ETHEREUM_PUBLIC_ADDRESS $BRIDGE_AMOUNT_WEI 2000000 800 \
   --value 5000000000000000000
 
-MM_INITIAL_BALANCE_L1=$(cast balance --rpc-url $ETHEREUM_RPC --ether $MM_ETHEREUM_WALLET_ADDRESS)
+export MM_AFTER_BALANCE_L1=$(cast balance --rpc-url $ETHEREUM_RPC --ether $MM_ETHEREUM_WALLET_ADDRESS)
 echo "After MM balance L1:"
-echo "$MM_INITIAL_BALANCE_L1"
+echo "$MM_AFTER_BALANCE_L1"
 
 echo "After MM balance L2:"
 npx zksync-cli wallet balance --rpc http://localhost:3050 --address "$MM_ZKSYNC_WALLET"
