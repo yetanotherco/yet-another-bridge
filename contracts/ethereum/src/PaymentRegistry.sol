6bada988e2d5c2137bcb94816a44d1125d942fef
Urix <43704209+uri-99@users.noreply.github.com>
2024-04-12 13:59:53 -0300

    feat(wip): adding support of ERC20 tokens.

--- contracts/ethereum/src/PaymentRegistry.sol
+++ contracts/ethereum/src/PaymentRegistry.sol
@@ -6,8 +6,12 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {IZkSync} from "@matterlabs/interfaces/IZkSync.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 
 contract PaymentRegistry is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+
+    using SafeERC20 for IERC20;
 
     event Transfer(uint256 indexed orderId, address srcAddress, address destAddress, uint256 amount, uint128 chainId);
     event ClaimPayment(uint256 indexed orderId, address destAddress, uint256 amount, uint128 chainId);
@@ -81,6 +85,48 @@ contract PaymentRegistry is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
         require(success, "Transfer failed.");
         emit Transfer(orderId, msg.sender, destAddress, msg.value, chainId); //2400 gas
+    }
+
+    function registerMMerc20Allowance(address erc20) external onlyOwnerOrMM() {
+        IERC20(erc20).safeIncreaseAllowance(PaymentRegistry, type(uint256).max);
+    }
+
+    function transferERC20(uint256 orderId, address destAddress, uint128 chainId, address erc20Address, uint256 amount) external onlyOwnerOrMM {
+        // Decide if MM fees are paid in ERC20 or in ETH.
+        // // If paid in ETH:
+        // // It is easy for MM to calculate how much fee is desirable for him to bridge the tokens
+        // // But An extra tx is needed containing this gas.
+        // // it is more expensive
+
+        // // If paid in ERC20:
+        // // MM can get paid in a random coin, price may vary, more difficult for MM to determine how much ERC20 tokens are necesarry.
+        // // But MM only subscribes to desired ERC20. For example if he only bridges USDT, he may be willing to take USDT as fee for bridge.
+        // // No extra tx is needed to pay this gas fee, MM will simply transfer less ERC20 tokens than what he recieved.
+        // // It is cheaper
+
+
+        // Transfer process:
+        // analyzed alternative:
+        // transfer from MM to PaymentRegistry
+        // transfer from PaymentRegistry to User
+        // // This solution makes 2 ERC20 transfers, very expensive and unnecessary
+        // IERC20(erc20Address).safeTransferFrom(msg.sender, PaymentRegistry, amount); //this needs allowance
+        // IERC20(erc20Address).safeTransfer(destAddress, amount); //from this contract. should recieve the erc20 tokens first. Calling a TransferFrom(MM, to YAB)
+
+        // Better alternative:
+        // directly transfer from MM to User
+        // // This solution seems appropriate. TransferFrom was built for exactly this.
+
+        // before transferFrom I should check funds + allowance? or safeTransfer checks this?
+        // // Funds:
+        // // We shouldnt check for funds: MM shouldn't call Transfer if he doesnt have enough ERC20 tokens for the bridge.
+
+        // // Allowance:
+        // // We have only 1 MM, we should allow PaymentRegistry to send all MM tokens. Set PaymentRegistry allowance of MM to unlimited.
+        // // This unlimited allowance should be set in a separate function. MM will allow to brdige x or y ERC20.
+        // // // we could even find new uses for this allowance. PaymentRegistry could be more intertwined with MM. Maybe automatically doing transfers in its name.
+        
+        IERC20(erc20Address).safeTransferFrom(msg.sender, destAddress, amount); //this needs allowance
     }
 
     function claimPaymentStarknet(uint256 orderId, address destAddress, uint256 amount) external payable onlyOwnerOrMM {
