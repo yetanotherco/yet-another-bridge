af2e7d112cd6061c79515f569446ef2008c9d3b3
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-04-12 19:05:15 -0300

    fix: change comment to standard python convention

--- mm-bot/src/models/zksync_log.py
+++ mm-bot/src/models/zksync_log.py
@@ -1,6 +1,8 @@
 from web3.types import EventData
 
 
-# EventData web3 library class doesn't have a from_address field, so we need to extend it to store it
 class ZksyncLog(EventData):
+    """
+    EventData web3 library class doesn't have a from_address field, so we need to extend it to store it
+    """
     from_address: str
