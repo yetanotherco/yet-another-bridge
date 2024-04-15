05b98370a843df06348f6fd2b23089a60d1e8661
Urix <43704209+uri-99@users.noreply.github.com>
2024-04-15 15:50:20 -0300

    refactor: remove claimPayment tx logs

--- .github/workflows/zksync-scripts/claim_payment.sh
+++ .github/workflows/zksync-scripts/claim_payment.sh
@@ -18,7 +18,7 @@ echo "Withdrawing $BRIDGE_AMOUNT_ETH ETH" # == $BRIDGE_AMOUNT_WEI WEI"
 cast send --rpc-url $ETHEREUM_RPC --private-key $ETHEREUM_PRIVATE_KEY --gas-price 2000000000 \
   $PAYMENT_REGISTRY_PROXY_ADDRESS "claimPaymentZKSync(uint256, address, uint256, uint256, uint256)" \
   "0" $USER_ETHEREUM_PUBLIC_ADDRESS $BRIDGE_AMOUNT_WEI 2000000 800 \
-  --value 5000000000000000000
+  --value 5000000000000000000 > /dev/null
 
 # echo "\nMM L1 funds after Claim Payment"
 # BALANCE_MM_L1_AFTER_CLAIMPAYMENT=$(cast balance --rpc-url $ETHEREUM_RPC $MM_ETHEREUM_WALLET_ADDRESS)
