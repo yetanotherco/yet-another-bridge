848a177ea804410a992a0d464f8853d945fe8489
Santos Rosati <65305492+srosati@users.noreply.github.com>
2024-04-16 18:00:23 -0300

    fix (mm-bot): estimate zksync message fee & gas limit (#241)
    * feat: fee estimate on ZKSync (WIP)
    
    * feat (zksync): estimate message fee & gas limit
    
    * refactor: remove debug print
    
    * refactor: fallback error message on estimate_gas_limit
    
    Co-authored-by: Julian Arce <52429267+JuArce@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Julian Arce <52429267+JuArce@users.noreply.github.com>

--- mm-bot/src/services/fee_calculators/zksync_fee_calculator.py
+++ mm-bot/src/services/fee_calculators/zksync_fee_calculator.py
@@ -1,22 +1,25 @@
 from zksync2.core.utils import DEPOSIT_GAS_PER_PUBDATA_LIMIT
+from web3 import Web3
+from zksync2.core.utils import apply_l1_to_l2_alias
+from zksync2.core.utils import DEPOSIT_GAS_PER_PUBDATA_LIMIT
+from zksync2.module.request_types import EIP712Meta
 
 from models.order import Order
 from services.fee_calculators.fee_calculator import FeeCalculator
+from services import ethereum, zksync
 
 
 class ZksyncFeeCalculator(FeeCalculator):
-
-    async def estimate_overall_fee(self, order: Order) -> int:
-        return 0  # TODO: implement
-
     async def estimate_message_fee(self, order: Order) -> int:
         """
         This fee is used as value in claim payment tx to ethereum
         """
-        return 10_000_000_000_000_000  # 0.01 ETH TODO implement estimation
-
+        l2_gas_limit = await self.estimate_gas_limit(order)
+        gas_price = ethereum.get_gas_price()
+        return await zksync.estimate_message_fee(gas_price, l2_gas_limit)
+    
     async def estimate_gas_limit(self, order: Order) -> int:
-        return 300_000
+        return await zksync.estimate_gas_limit(order.recipient_address)
 
     def estimate_gas_per_pub_data_byte_limit(self, order: Order) -> int:
         return DEPOSIT_GAS_PER_PUBDATA_LIMIT  # This is a constant from zksync2.core.utils
