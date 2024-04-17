f57cbdc54b2f772f0c32d3e97925b5fbeee21d5b
JuArce <52429267+JuArce@users.noreply.github.com>
2024-04-16 22:54:51 -0300

    chore(mm-bot): update claim payment fees for each L2

--- mm-bot/src/services/fee_calculators/starknet_fee_calculator.py
+++ mm-bot/src/services/fee_calculators/starknet_fee_calculator.py
@@ -3,10 +3,20 @@ from starknet_py.hash.selector import get_selector_from_name
 from config import constants
 from models.order import Order
 from services import starknet
+from services.ethereum import get_gas_price
 from services.fee_calculators.fee_calculator import FeeCalculator
 
 
 class StarknetFeeCalculator(FeeCalculator):
+
+    def estimate_claim_payment_fee(self) -> int:
+        """
+        Due to the deposit does not exist on ethereum at this point,
+        we cannot estimate the gas fee of the claim payment transaction
+        So we will use fixed values for the gas
+        """
+        eth_claim_payment_gas = 94642  # TODO this is a fixed value, if the contract changes, this should be updated
+        return eth_claim_payment_gas * get_gas_price()
 
     async def estimate_message_fee(self, order: Order) -> int:
         """
