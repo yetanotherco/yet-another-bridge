7a8f372e2430bd2916ef0544b8eded2c2292042c
Urix <43704209+uri-99@users.noreply.github.com>
2024-04-18 19:25:48 -0300

    test(wip): zksync unit tests

--- contracts/zksync/contracts/ERC20_L2.sol
+++ contracts/zksync/contracts/ERC20_L2.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// contract UriCoin is ERC20 {
+//     constructor() ERC20("UriCoin", "Uri") {
+//         _mint(0xB321099cf86D9BB913b891441B014c03a6CcFc54, 1000000); //hardcoded recipient + initial_supply
+//     }
+// }
+
 contract UriCoin is ERC20 {
-    constructor() ERC20("UriCoin", "Uri") {
-        _mint(0xB321099cf86D9BB913b891441B014c03a6CcFc54, 1000000); //hardcoded recipient + initial_supply
+    constructor(address initial_whale, uint256 initial_supply) ERC20("UriCoin", "Uri") {
+        _mint(initial_whale, initial_supply); //hardcoded recipient + initial_supply
     }
 }
--- contracts/zksync/test/erc20.test.ts
+++ contracts/zksync/test/erc20.test.ts
@@ -5,6 +5,8 @@ import { getWallet, deployContract, LOCAL_RICH_WALLETS, getProvider } from '../d
 
 let provider: Provider;
 let escrow: Contract;
+let erc20_l2: Contract;
+let erc20_l1: Contract;
 let paymentRegistry: Wallet = getWallet(LOCAL_RICH_WALLETS[3].privateKey); //its Wallet data type because I will mock calls from this addr
 
 let deployer: Wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
@@ -17,27 +19,91 @@ let user_eth2: Wallet = getWallet(LOCAL_RICH_WALLETS[2].privateKey);
 
 const fee = 1; //TODO check, maybe make fuzz
 const value = 10; //TODO check, maybe make fuzz
+const amount_l2 = 20;
+const amount_l1 = 10;
+
+const erc20_l1_address = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+let erc20_l2_address: string;
+
+const initial_erc20_balance = 1000000;
 
 beforeEach( async () => {
   escrow = await deployAndInit();
-  // erc20 = await deployContract("ERC20", deployer, [1000]);
+  erc20_l2 = await deployContract("UriCoin", [user_zk.address, initial_erc20_balance], { wallet: deployer });
+  erc20_l2_address = await erc20_l2.getAddress();
+  
   provider = getProvider();
 });
 
+//working:
+// describe('ERC20 basic tests', function () {
+//   it("Should airdrop correctly", async () => {
+//     const balanceOf = await erc20.balanceOf(user_zk.address);
+//     expect(balanceOf).to.eq(initial_erc20_balance)
+//   });
+
+//   it("Should transfer correctly", async () => {
+//     const transferTx = await erc20.connect(user_zk).transfer(user_zk2.address, value);
+//     await transferTx.wait();
+
+//     const balanceOf = await erc20.balanceOf(user_zk2.address);
+//     expect(balanceOf).to.eq(value)
+//   });
+
+//   it("Should not transfer more than balance", async () => {
+//     expect(erc20.connect(user_zk).transfer(user_zk2.address, initial_erc20_balance+1)).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+//   });
+
+//   it("Should not transfer without allowance", async () => {
+//     expect(erc20.connect(user_zk2).transferFrom(user_zk.address, user_zk2.address, value)).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+//   });
+
+//   it("Should approve correctly", async () => {
+//     const approveTx = await erc20.connect(user_zk).approve(user_zk2.address, value);
+//     await approveTx.wait();
+
+//     const allowance = await erc20.allowance(user_zk.address, user_zk2.address);
+//     expect(allowance).to.eq(value)
+//   });
+
+//   it("Should transferFrom correctly", async () => {
+//     const approveTx = await erc20.connect(user_zk).approve(user_zk2.address, value);
+//     await approveTx.wait();
+
+//     const transferFromTx = await erc20.connect(user_zk2).transferFrom(user_zk.address, user_zk2.address, value);
+//     await transferFromTx.wait();
+
+//     const balanceOf = await erc20.balanceOf(user_zk2.address);
+//     expect(balanceOf).to.eq(value)
+//   });
+// })
 
 
 describe('ERC20 Set Order tests', function () {
+  it("Should emit correct Event", async () => {
+    const approveTx = await erc20_l2.connect(user_zk).approve(await erc20_l2.getAddress(), amount_l2);
+    await approveTx.wait();
+
+    const allowance = await erc20_l2.allowance(user_zk.address, await erc20_l2.getAddress());
+    expect(allowance).to.eq(amount_l2)
+
+    //contract caller is not being correctly set.
+    const setOrderTx = await escrow.connect(user_zk).set_order_erc20(user_eth.address, amount_l2, amount_l1, await erc20_l2.getAddress(), erc20_l1_address, {value: fee});
+
+    await expect(setOrderTx)
+      .to.emit(escrow, "SetOrder").withArgs(0, user_eth.address, amount_l2, amount_l1, fee, erc20_l2_address, erc20_l1_address);
+  });
   // it("Should emit correct Event", async () => {
   //   const setOrderTx = await escrow.connect(user_zk).set_order_erc20(user_eth, fee, {value});
 
   //   await expect(setOrderTx)
-  //     .to.emit(escrow, "SetOrder").withArgs(0, user_eth, value-fee, fee)
+  //     .to.emit(escrow, "SetOrder").withArgs(0, user_eth, value-fee, fee);
   // });
   // it("Should get the order setted", async () => {
   //   const setOrderTx = await escrow.connect(user_zk).set_order(user_eth, fee, {value});
 
   //   await expect(setOrderTx)
-  //     .to.emit(escrow, "SetOrder").withArgs(0, user_eth, value-fee, fee)
+  //     .to.emit(escrow, "SetOrder").withArgs(0, user_eth, value-fee, fee);
 
   //   const newOrder = await escrow.get_order(0);
 
