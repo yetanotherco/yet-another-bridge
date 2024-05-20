aa47f6c9276053d878062d6b3771671e3943bbf9
santiagohirsch <shirsch@itba.edu.ar>
2024-05-20 10:33:50 -0300

    chore: add testing configuration

---
+++ mm-bot/src/config/testing_config.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import constants
+
+Engine = create_engine(
+   constants.SQL_LITE_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
+
+Base = declarative_base()
